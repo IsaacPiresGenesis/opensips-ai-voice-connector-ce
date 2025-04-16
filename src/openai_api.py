98546ea8f6ef8df64cf9c4c6bcc49846e03b5ad6
Isaac Pires <isaac@genesistelecom.com.br>
2025-04-16 17:42:05 -0300

    sds

--- src/openai_api.py
+++ src/openai_api.py
@@ -109,8 +109,7 @@ class OpenAI(AIEngine):  # pylint: disable=too-many-instance-attributes
         }
         logging.info("URL ==> " + self.url)
         logging.info("HEADERS ==> " + json.dumps(headers))
-        coro = connect(self.url, additional_headers=headers)
-        self.ws = await coro
+        self.ws = await connect(self.url, additional_headers=headers)
         # self.ws = websocket.WebSocketApp(
         #     self.url,
         #     header=openai_headers,
