ad42d24bb21a78a5cc3dc406e0fd9ae038a9694f
Isaac Pires <isaac@genesistelecom.com.br>
2025-04-16 08:24:40 -0300

    update

--- src/openai_api.py
+++ src/openai_api.py
@@ -28,7 +28,7 @@ import base64
 import logging
 import asyncio
 from queue import Empty
-import websockets
+from websockets.asyncio.client import connect
 from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError
 from ai import AIEngine
 from codec import get_codecs, CODECS, UnsupportedCodec
@@ -92,7 +92,7 @@ class OpenAI(AIEngine):  # pylint: disable=too-many-instance-attributes
                 "OpenAI-Beta": "realtime=v1"
         }
         logging.info(" OPENAI_API -> conectando ao websocket ")
-        self.ws = await websockets.connect(self.url, additional_headers=openai_headers)
+        self.ws = await connect(self.url, additional_headers=openai_headers)
         # self.ws = websocket.WebSocketApp(
         #     self.url,
         #     header=openai_headers,
